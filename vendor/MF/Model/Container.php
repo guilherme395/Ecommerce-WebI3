523dcc3cf3064efd3ba83c5157e1f57879f4716d
Guilherme Lima <guilhermelimaleite06@gmail.com>
2023-01-10 19:00:44 -0300

    ...

--- vendor/MF/Model/Container.php
+++ vendor/MF/Model/Container.php
@@ -8,6 +8,14 @@ class Container
 {
     public static function getModel($model)
     {
+        /**
+         * Instanciar classes de Create, Read, Update e Delete no momento de execução da model.
+         * 
+         * $create = new Create
+         * $read = new Read
+         * $update = new Update
+         * $delete = new Delete
+         */
         $class = "App\\Models\\" . ucfirst($model);
         return new $class(Db::getDb());
     }
